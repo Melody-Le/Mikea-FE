9742a610eff52e64632ac1c9dc6f6642fd20eff0
MUI LE <tanmui1992@gmail.com>
2022-11-04 15:17:27 +0800

    Feat: update axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-// const BASE_URL = "https://mikea-ikea-clone.herokuapp.com/api/v1";
-const BASE_URL = "http://localhost:8800/api/v1";
+const BASE_URL = "https://mikea-ikea-clone.herokuapp.com/api/v1";
+// const BASE_URL = "http://localhost:8800/api/v1";
 
 export default axios.create({
   baseURL: BASE_URL,
